0072f1dcb2aea4ede8b2549d944e1b57fab3a11b
prem <prem@cortex.cs.northwestern.edu>
2018-10-23 02:20:20 -0500

    adding buckets

--- handler.py
+++ handler.py
@@ -2,13 +2,21 @@ import numpy as np
 from museval import eval_dir
 import zipfile
 import json
+import os
+
+def unzip(zip_path):
+	zip_ref = zipfile.ZipFile(zip_path, 'r')
+	zip_ref.extractall('/tmp/')
+	zip_ref.close()
 
 def main(event, context):
+	print(event)
 	print("Evaluating estimates")
-	scores = eval_dir('eval/references', 'eval/estimates')
+	unzip('eval.zip')
+	scores = eval_dir('/tmp/eval/references', '/tmp/eval/estimates')
+	print(scores)
 	print("Writing to scores.json")
-	with ("scores.json", "w") as f:
-		json.dump(scores.scores, f, indent=2, allow_nan=True)
+	return scores.json
 
 if __name__ == "__main__":
 	main('','')
